f9e7d7dcb273bba9664e55777a6cc0c552b14a71
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-25 13:45:42 -0500

    created new feature test for dynamic data listing

--- spec/web/features/list_tweets_spec.rb
+++ spec/web/features/list_tweets_spec.rb
@@ -1,9 +1,17 @@
 require 'features_helper'
 
 RSpec.describe 'lists tweets' do
-  it 'displays the tweets' do
+   let(:repository) {TweetRepository.new}
+  before do
+    repository.clear
+
+    repository.create(username: 'Doggo', tweet: 'Hungry for snacks...')
+    repository.create(username: 'Fido', tweet: 'Just found out im adopted!')
+  end
+
+  it 'Displays the tweets on the page' do
     visit '/tweets'
-    
+
     #within an element with the id of tweets
     within '#tweets' do
       #expect to find two elements with the class of tweet
