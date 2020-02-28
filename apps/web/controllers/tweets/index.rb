a12ec26f2a1b03c061fce1a2a7718be51a07a348
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-28 15:18:44 -0500

    Backend can now send data via json

--- apps/web/controllers/tweets/index.rb
+++ apps/web/controllers/tweets/index.rb
@@ -3,11 +3,22 @@ module Web
     module Tweets
       class Index
         include Web::Action
-        
+        require 'json'        
         expose :tweets
 
         def call(params)
           @tweets = TweetRepository.new.all
+          
+          response = []
+          
+          tweets.each do |tweet|
+            response << tweet.to_h
+          end
+
+          puts response
+
+          self.body = JSON.generate(response)
+
         end
       end
     end
