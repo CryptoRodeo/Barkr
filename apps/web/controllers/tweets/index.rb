71b185f2679049550458dd5ef5c188513d0acf5d
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-06 14:24:18 -0500

    testing out ways to get user ip address

--- apps/web/controllers/tweets/index.rb
+++ apps/web/controllers/tweets/index.rb
@@ -3,11 +3,22 @@ module Web
     module Tweets
       class Index
         include Web::Action
-        require 'json'        
+        require 'open-uri'
+        require 'json'
+
+        attr_accessor :ip
         expose :tweets
 
         def call(params)
           @tweets = TweetRepository.new.all
+          
+          location = URI.open('https://ipapi.co/json/').read
+         @ip = Hash(JSON.parse(loc, :symbolize_names => true))
+          
+         puts "IP ADDRESS: #{self.ip[:ip]}"
+          puts "From Hanami: #{request.ip}" # performance degragating, not recommended.
+          puts "From Rack: #{params.env["REMOTE_ADDR"]}"
+        
         end
       end
     end
