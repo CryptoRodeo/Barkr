25753d4839ab11f55dc2e0d7099bdf621121a422
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-07 07:50:05 -0500

    cleaning up delete controller

--- apps/web/controllers/tweets/delete.rb
+++ apps/web/controllers/tweets/delete.rb
@@ -4,8 +4,13 @@ module Web
             class Delete
                 include Web::Action
 
+                attr_accessor :tweet_repo
+
+                def initialize(tweet_repo = TweetRepository.new)
+                    @tweet_repo ||= tweet_repo
+                end
+
                 def call(params)
-                    tweet_repo = TweetRepository.new
                     tweet_repo.delete(params[:id])
                     redirect_to '/tweets/'
                 end
