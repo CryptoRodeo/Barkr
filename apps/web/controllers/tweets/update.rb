e213fe4f93b8ea7abb964afe49e3e100c0b21074
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-07 08:09:16 -0500

    cleaning up update controller

--- apps/web/controllers/tweets/update.rb
+++ apps/web/controllers/tweets/update.rb
@@ -5,15 +5,24 @@ module Web
                 include Web::Action
                 
                 expose :updated_tweet
-                
-                def call(params)
-                    tweet_repo = TweetRepository.new
+
+                attr_accessor :updated_tweet, :tweet_repo
+                def initialize(tweet_repo = TweetRepository.new)
+                    @tweet_repo ||= tweet_repo
+                end
+
+                def update_tweet 
                     unless params[:tweet_edited].nil?
                         updated = params[:tweet_edited]
                         @updated_tweet = tweet_repo.update(params[:id], username: updated[:username], content: updated[:content])
                     else
                         @updated_tweet = tweet_repo.find(params[:id])
                     end
+                end
+
+                
+                def call(params)
+                    update_tweet
                     redirect_to "/tweets/#{params[:id]}"
                 end
             end
