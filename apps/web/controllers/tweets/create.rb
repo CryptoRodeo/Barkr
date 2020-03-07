9cb266f0f38233cf745a6a4af992f2cbd69c5693
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-07 07:47:29 -0500

    cleaning up create controller

--- apps/web/controllers/tweets/create.rb
+++ apps/web/controllers/tweets/create.rb
@@ -6,19 +6,32 @@ module Web
         
         expose :tweet
         
-        # DRY validations for the form inputs.
-        params do
-          required(:tweet).schema do
-            required(:username).filled(:str?)
-            required(:content).filled(:str?)
+        def initialize(tweet = TweetRepository.new)
+          @tweet ||= tweet
+        end
+
+        def validate_inputs
+          # DRY validations for the form inputs.
+          params do
+            required(:tweet).schema do
+              required(:username).filled(:str?)
+              required(:content).filled(:str?)
+            end
           end
         end
 
+        def create_tweet
+          @tweet.create(params[:tweet])
+        end
+
+        def inputs_valid?
+          params.valid?
+        end
 
         def call(params)
-          if params.valid?
-            @tweet = TweetRepository.new.create(params[:tweet])
-
+          validate_inputs
+          if inputs_valid?
+            create_tweet
             redirect_to '/tweets'
           else
             self.status = 422 #set to 422, pass control to view.
--- apps/web/controllers/tweets/index.rb
+++ apps/web/controllers/tweets/index.rb
@@ -5,20 +5,17 @@ module Web
         include Web::Action
         require 'open-uri'
         require 'json'
-
-        attr_accessor :ip
+       
+        attr_accessor :user, :tweets
         expose :tweets
+        
+        def initialize(tweets = TweetRepository.new.all)
+          @user = {}
+          @tweets ||= tweets
+        end
 
         def call(params)
-          @tweets = TweetRepository.new.all
-          
-          location = URI.open('https://ipapi.co/json/').read
-         @ip = Hash(JSON.parse(location, :symbolize_names => true))
-          
-         puts "IP ADDRESS: #{self.ip[:ip]}"
-          puts "From Hanami: #{request.ip}" # performance degragating, not recommended.
-          puts "From Rack: #{params.env["REMOTE_ADDR"]}"
-        
+          @user[:ip_address] = params.env["REMOTE_ADDR"]
         end
       end
     end
