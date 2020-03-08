879946b77e3e29c3e52735fb8a53f53a4f70d00b
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-08 15:13:11 -0400

    Better error handling:

--- apps/web/controllers/tweets/read.rb
+++ apps/web/controllers/tweets/read.rb
@@ -7,33 +7,21 @@ module Web
                 
                 attr_accessor :tweet, :tweet_repo
                 expose :tweet
-
-
+                
                 def initialize(tweet_repo = TweetRepository.new)
                     @tweet_repo ||= tweet_repo
-                    @uuid = {
-                        :v4 => /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-                    }
                 end
 
-                def UUID(version)
-                    @uuid[version.downcase.to_sym]
-                end
-               
-                #checks to see if the id retrieved is valid UUID V4
                 def id_valid?
-                    params[:id].match?(UUID("v4"))
-                end
-
-                def find_tweet
-                    unless id_valid?
-                        redirect_to '/tweets/'
+                    begin
+                        tweet_repo.find(params[:id])
+                    rescue 
+                        halt 500, "Invalid ID"
                     end
-                    @tweet = tweet_repo.find(params[:id])
                 end
 
                 def call(params)
-                    halt 500 unless find_tweet
+                    @tweet = tweet_repo.find(params[:id]) if id_valid?
                 end
             end
         end
