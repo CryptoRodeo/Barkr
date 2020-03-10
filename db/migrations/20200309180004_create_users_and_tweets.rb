ab81b7bea0f7fae7d4f7233713c46f628d95e517
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-10 14:41:12 -0400

    fixed db migration issue

--- db/migrations/20200309180004_create_users_and_tweets.rb
+++ db/migrations/20200309180004_create_users_and_tweets.rb
@@ -1,6 +1,13 @@
 Hanami::Model.migration do
-  up do
+  change do
     execute 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
+    create_table :users do
+      primary_key :id, 'uuid', null:false, default: Hanami::Model::Sql.function(:uuid_generate_v4)
+
+      column :ip, String, unique: true, null:false, size: 48
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
 
     create_table :tweets do
       primary_key :id, 'uuid', null:false, default: Hanami::Model::Sql.function(:uuid_generate_v4)
@@ -14,7 +21,8 @@ Hanami::Model.migration do
   end
 
     down do
+      drop_table :users 
       drop_table :tweets
       execute 'DROP EXTENSION IF EXISTS "uuid-ossp"'
+    end
   end
-end
