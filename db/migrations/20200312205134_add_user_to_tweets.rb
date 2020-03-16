54724b7c2f9e7ef88cf1356b5cedb4c72f0c54e2
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-16 08:58:31 -0400

    forgot to actually save changes...

--- db/migrations/20200312205134_add_user_to_tweets.rb
+++ db/migrations/20200312205134_add_user_to_tweets.rb
@@ -1,7 +1,7 @@
 Hanami::Model.migration do
   change do
     alter_table :tweets do
-      add_foreign_key :created_by, :users, key: :id, type: 'uuid', on_delete: :cascade,  null: false
+      add_foreign_key :user_id, :users, key: :id, type: 'uuid', on_delete: :cascade,  null: false
     end
   end
 end
